04816afbd3376937c7ad70413b591f8612582d12
amin <amin@ahmadizadeh.ir>
2020-09-25 02:51:40 -0700

    first commit

---
+++ assets/js/loadParts.js
@@ -0,0 +1,6 @@
+$(function(){
+    $("#header").load("/assets/theme/header.html");
+    $("#menu").load("/assets/theme/menu.html");
+    $("#search").load("/assets/theme/search.html");
+    $("#footer").load("/assets/theme/footer.html");
+});
